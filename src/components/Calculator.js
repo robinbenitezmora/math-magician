89a4955368fc1e32a6dc17c38b80f3e72a285d7e
robinbenitezmora <robinbenitez1990@gmail.com>
2022-09-07 16:02:15 -0500

    Update 'operate.js' file in order to apply 'Big' class

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import calculate from '../logic/calculate';
 // eslint-disable-next-line react/prefer-stateless-function
 class Calculator extends React.Component {
   render() {
