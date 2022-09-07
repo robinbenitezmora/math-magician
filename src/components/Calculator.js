02290b37608508b281550b6f1538ea47ab3e170e
robinbenitezmora <robinbenitez1990@gmail.com>
2022-09-07 10:30:41 -0500

    Add code lines in order to give style to the project

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -4,38 +4,38 @@ class Calculator extends React.Component {
   render() {
     return (
       <div className="calculator">
-        <div className="calculator__display">
-          <div className="calculator__display-result">0</div>
+        <div className="calculator-display">
+          <div className="calculator-display-result">0</div>
         </div>
-        <div className="calculator__buttons">
-          <div className="calculator__buttons-row">
-            <button type="submit">AC</button>
-            <button type="submit">+/-</button>
-            <button type="submit">%</button>
-            <button type="submit" className="operation">÷</button>
+        <div className="calculator-buttons">
+          <div className="calculator-buttons-row">
+            <button type="submit" className="btn">AC</button>
+            <button type="submit" className="btn">+/-</button>
+            <button type="submit" className="btn">%</button>
+            <button type="submit" className="btn-operation">÷</button>
           </div>
-          <div className="calculator__buttons-row">
-            <button type="submit">7</button>
-            <button type="submit">8</button>
-            <button type="submit">9</button>
-            <button type="submit" className="operation">x</button>
+          <div className="calculator-buttons-row">
+            <button type="submit" className="btn">7</button>
+            <button type="submit" className="btn">8</button>
+            <button type="submit" className="btn">9</button>
+            <button type="submit" className="btn-operation">x</button>
           </div>
-          <div className="calculator__buttons-row">
-            <button type="submit">4</button>
-            <button type="submit">5</button>
-            <button type="submit">6</button>
-            <button type="submit" className="operation">-</button>
+          <div className="calculator-buttons-row">
+            <button type="submit" className="btn">4</button>
+            <button type="submit" className="btn">5</button>
+            <button type="submit" className="btn">6</button>
+            <button type="submit" className="btn-operation">-</button>
           </div>
-          <div className="calculator__buttons-row">
-            <button type="submit">1</button>
-            <button type="submit">2</button>
-            <button type="submit">3</button>
-            <button type="submit" className="operation">+</button>
+          <div className="calculator-buttons-row">
+            <button type="submit" className="btn">1</button>
+            <button type="submit" className="btn">2</button>
+            <button type="submit" className="btn">3</button>
+            <button type="submit" className="btn-operation">+</button>
           </div>
-          <div className="calculator__buttons-row">
-            <button type="submit" className="zero">0</button>
-            <button type="submit">.</button>
-            <button type="submit" className="operation">=</button>
+          <div className="calculator-buttons-row">
+            <button type="submit" className="btn zero">0</button>
+            <button type="submit" className="btn">.</button>
+            <button type="submit" className="btn-operation">=</button>
           </div>
         </div>
       </div>
