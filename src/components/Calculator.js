74dfbd183165b0225ef3f4e63c483ccaebcd78ec
robinbenitezmora <robinbenitez1990@gmail.com>
2022-09-08 06:43:58 -0500

    Edit code lines inside 'Calculator.js' file

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -2,41 +2,59 @@ import React from 'react';
 import calculate from '../logic/calculate';
 // eslint-disable-next-line react/prefer-stateless-function
 class Calculator extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+    this.eventCalculator = this.eventCalculator.bind(this);
+  }
+
+  eventCalculator(event) {
+    const { target } = event;
+    const { name } = target;
+    const { value } = target;
+    const { state } = this;
+    const data = calculate(state, { name, value });
+    this.setState(data);
+  }
+
   render() {
+    const { total, next, operation } = this.state;
     return (
       <div className="calculator">
         <div className="calculator-display">
-          <div className="calculator-display-result">0</div>
+          {total || '0'}
+          {operation || ''}
+          {next || ''}
         </div>
         <div className="calculator-buttons">
           <div className="calculator-buttons-row">
-            <button type="submit" className="btn">AC</button>
-            <button type="submit" className="btn">+/-</button>
-            <button type="submit" className="btn">%</button>
-            <button type="submit" className="btn-operation">÷</button>
+            <button type="submit" onClick={this.eventCalculator} className="btn">AC</button>
+            <button type="submit" onClick={this.eventCalculator} className="btn">+/-</button>
+            <button type="submit" onClick={this.eventCalculator} className="btn">%</button>
+            <button type="submit" onClick={this.eventCalculator} className="btn-operation">÷</button>
           </div>
           <div className="calculator-buttons-row">
-            <button type="submit" className="btn">7</button>
-            <button type="submit" className="btn">8</button>
-            <button type="submit" className="btn">9</button>
-            <button type="submit" className="btn-operation">x</button>
+            <button type="submit" onClick={this.eventCalculator} className="btn">7</button>
+            <button type="submit" onClick={this.eventCalculator} className="btn">8</button>
+            <button type="submit" onClick={this.eventCalculator} className="btn">9</button>
+            <button type="submit" onClick={this.eventCalculator} className="btn-operation">x</button>
           </div>
           <div className="calculator-buttons-row">
-            <button type="submit" className="btn">4</button>
-            <button type="submit" className="btn">5</button>
-            <button type="submit" className="btn">6</button>
-            <button type="submit" className="btn-operation">-</button>
+            <button type="submit" onClick={this.eventCalculator} className="btn">4</button>
+            <button type="submit" onClick={this.eventCalculator} className="btn">5</button>
+            <button type="submit" onClick={this.eventCalculator} className="btn">6</button>
+            <button type="submit" onClick={this.eventCalculator} className="btn-operation">-</button>
           </div>
           <div className="calculator-buttons-row">
-            <button type="submit" className="btn">1</button>
-            <button type="submit" className="btn">2</button>
-            <button type="submit" className="btn">3</button>
-            <button type="submit" className="btn-operation">+</button>
+            <button type="submit" onClick={this.eventCalculator} className="btn">1</button>
+            <button type="submit" onClick={this.eventCalculator} className="btn">2</button>
+            <button type="submit" onClick={this.eventCalculator} className="btn">3</button>
+            <button type="submit" onClick={this.eventCalculator} className="btn-operation">+</button>
           </div>
           <div className="calculator-buttons-row">
-            <button type="submit" className="btn zero">0</button>
-            <button type="submit" className="btn">.</button>
-            <button type="submit" className="btn-operation">=</button>
+            <button type="submit" onClick={this.eventCalculator} className="btn zero">0</button>
+            <button type="submit" onClick={this.eventCalculator} className="btn">.</button>
+            <button type="submit" onClick={this.eventCalculator} className="btn-operation">=</button>
           </div>
         </div>
       </div>
