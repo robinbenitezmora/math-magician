efd4aa56d31c9b7cfa26cc1121e4fbc48dbc6f7d
robinbenitezmora <robinbenitez1990@gmail.com>
2022-09-08 07:08:05 -0500

    Add 'reportWebVitals.js' file

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -28,33 +28,33 @@ class Calculator extends React.Component {
         </div>
         <div className="calculator-buttons">
           <div className="calculator-buttons-row">
-            <button type="submit" onClick={this.eventCalculator} className="btn">AC</button>
-            <button type="submit" onClick={this.eventCalculator} className="btn">+/-</button>
-            <button type="submit" onClick={this.eventCalculator} className="btn">%</button>
-            <button type="submit" onClick={this.eventCalculator} className="btn-operation">÷</button>
+            <button type="button" onClick={this.eventCalculator} className="btn">AC</button>
+            <button type="button" onClick={this.eventCalculator} className="btn">+/-</button>
+            <button type="button" onClick={this.eventCalculator} className="btn">%</button>
+            <button type="button" onClick={this.eventCalculator} className="btn-operation">÷</button>
           </div>
           <div className="calculator-buttons-row">
-            <button type="submit" onClick={this.eventCalculator} className="btn">7</button>
-            <button type="submit" onClick={this.eventCalculator} className="btn">8</button>
-            <button type="submit" onClick={this.eventCalculator} className="btn">9</button>
-            <button type="submit" onClick={this.eventCalculator} className="btn-operation">x</button>
+            <button type="button" onClick={this.eventCalculator} className="btn">7</button>
+            <button type="button" onClick={this.eventCalculator} className="btn">8</button>
+            <button type="button" onClick={this.eventCalculator} className="btn">9</button>
+            <button type="button" onClick={this.eventCalculator} className="btn-operation">x</button>
           </div>
           <div className="calculator-buttons-row">
-            <button type="submit" onClick={this.eventCalculator} className="btn">4</button>
-            <button type="submit" onClick={this.eventCalculator} className="btn">5</button>
-            <button type="submit" onClick={this.eventCalculator} className="btn">6</button>
-            <button type="submit" onClick={this.eventCalculator} className="btn-operation">-</button>
+            <button type="button" onClick={this.eventCalculator} className="btn">4</button>
+            <button type="button" onClick={this.eventCalculator} className="btn">5</button>
+            <button type="button" onClick={this.eventCalculator} className="btn">6</button>
+            <button type="button" onClick={this.eventCalculator} className="btn-operation">-</button>
           </div>
           <div className="calculator-buttons-row">
-            <button type="submit" onClick={this.eventCalculator} className="btn">1</button>
-            <button type="submit" onClick={this.eventCalculator} className="btn">2</button>
-            <button type="submit" onClick={this.eventCalculator} className="btn">3</button>
-            <button type="submit" onClick={this.eventCalculator} className="btn-operation">+</button>
+            <button type="button" onClick={this.eventCalculator} className="btn">1</button>
+            <button type="button" onClick={this.eventCalculator} className="btn">2</button>
+            <button type="button" onClick={this.eventCalculator} className="btn">3</button>
+            <button type="button" onClick={this.eventCalculator} className="btn-operation">+</button>
           </div>
           <div className="calculator-buttons-row">
-            <button type="submit" onClick={this.eventCalculator} className="btn zero">0</button>
-            <button type="submit" onClick={this.eventCalculator} className="btn">.</button>
-            <button type="submit" onClick={this.eventCalculator} className="btn-operation">=</button>
+            <button type="button" onClick={this.eventCalculator} className="btn zero">0</button>
+            <button type="button" onClick={this.eventCalculator} className="btn">.</button>
+            <button type="button" onClick={this.eventCalculator} className="btn-operation">=</button>
           </div>
         </div>
       </div>
