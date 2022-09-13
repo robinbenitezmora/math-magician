e4e2abefeb2100dbc62ef588faff32b334d72894
robinbenitezmora <robinbenitez1990@gmail.com>
2022-09-13 15:00:41 -0500

    Add javascript code in the modules

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 /*eslint-disable*/
 import * as React from 'react';
-import { Routes, Route, Router } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './components/Home';
 import Calculator from './components/Calculator';
 import Quote from './components/Quote';
