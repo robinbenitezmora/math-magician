f9d2f48ca7fe6dcaa297b1b629652f09e27bebc8
robinbenitezmora <robinbenitez1990@gmail.com>
2022-09-13 18:52:22 -0500

    Add styles to the complete pages of the project

--- src/components/Quote.js
+++ src/components/Quote.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Quote() {
   return (
     <div className="quote">
-      <h2 className="quote-title">Mathematics is not about numbers, equations, computations, or algorithms: it is about understanding. –William Paul Thurston</h2>
+      <p className="quote-title">Mathematics is not about numbers, equations, computations, or algorithms: it is about understanding. –William Paul Thurston</p>
     </div>
   );
 }
